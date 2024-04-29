41216d94e5e6284d4c3e6a79d08a0d58d08d5252
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2024-04-29 14:19:57 -0400

    ADD INSTANTIATION MATRIX2 UNIT TESTS
    Add tests to test empty matrix2 creation

---
+++ UnitTest/Source/Tests/MatrixTest.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_approx.hpp>
+
+#define GLM_FORCE_SILENT_WARNINGS
+#include <glm/glm.hpp>
+
+#include "libmath/Arithmetic.h"
+#include "libmath/Matrix.h"
+
+#define DISPLAY_MAT2(obj, title)\
+std::cout << title << ":\n" << obj[0][0] << ' ' << obj[0][1]\
+<< '\n' << obj[1][0] << ' ' << obj[1][1] << '\n';
+
+#define CHECK_MAT2(mat1, mat2)\
+CHECK(mth::almostEqual(mat1[0][0], mat2[0][0]));\
+CHECK(mth::almostEqual(mat1[0][1], mat2[0][1]));\
+CHECK(mth::almostEqual(mat1[1][0], mat2[1][0]));\
+CHECK(mth::almostEqual(mat1[1][1], mat2[1][1]))
+
+
+TEST_CASE("Matrix2", "[matrix] [all]")
+{
+    SECTION("Instanciation")
+    {
+        {
+            mth::Matrix2    myMat;
+            glm::mat2       glmMat(0.f);
+
+            DISPLAY_MAT2(myMat, "my empty matrix");
+            DISPLAY_MAT2(glmMat, "glm empty matrix");
+
+            CHECK_MAT2(myMat, glmMat);
+            
+        }
+    }
+}
