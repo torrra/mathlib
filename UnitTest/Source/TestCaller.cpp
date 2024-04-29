25be3a61bb8aab39eb63914fd50a63a9bcffa664
Noah DE PISCHOF <n.depischof@student.isartdigital.com>
2024-04-28 23:00:25 -0400

    CREATE TESTCALLER FILES
    Creare TestCaller.h and
    TestCaller.cpp to avoid going
    through the hassle of using Visual
    Studio's command line property
    (Windows only)

---
+++ UnitTest/Source/TestCaller.cpp
@@ -0,0 +1,12 @@
+#include "TestCaller.h"
+
+void AddTests(std::vector<const char*> tests)
+{
+    tests.push_back("[all]");
+
+    // tests.push_back("[angle]");
+    // tests.push_back("[vector]");
+
+    // tests.push_back("[intersection]");
+
+}
