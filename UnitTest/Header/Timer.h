4abddedff926576a53d1db4f7c2aee15d20e084c
Noah DE PISCHOF <141101956+torrra@users.noreply.github.com>
2024-05-19 21:42:15 -0400

    EDIT TIMER CLASS
    Remove Doxygen comments and use variables
    instead of magic numbers in Timer.h

--- UnitTest/Header/Timer.h
+++ UnitTest/Header/Timer.h
@@ -6,32 +6,31 @@
 
 using hrc = std::chrono::high_resolution_clock;
 
-/// @brief timer class to act as a stopwatch
+// Timer class to act as a stopwatch
 class Timer
 {
 public:
 	Timer() = default;
 	~Timer() = default;
 
-	/// @brief Start timer
+	// Start timer
 	void start(void)
 	{
 		m_beg = hrc::now();
 	}
 
-	/// @brief Stop timer and save time between start and stop
+	// Stop timer and save time between start and stop
 	void stop(void)
 	{
 		m_elapsed += std::chrono::duration_cast<std::chrono::nanoseconds>(hrc::now() - m_beg).count();
 	}
 
-	/// @brief Reset elapsed time
+	// Reset elapsed time
 	void clear(void) { m_elapsed = 0ll; }
 
 
-	/// @brief Display saved elapsed
-	/// @param verbose : Display a longer message
-	void display(bool verbose = false)
+	//  Display saved elapsed
+	void display(bool verbose = false) const
 	{
 		// Display time in nanoseconds only
 		if (!verbose)
@@ -40,41 +39,35 @@ public:
 		// Longer display
 		else
 		{
-			// Divide into microseconds, milliseconds, etc
-			long long		seconds = static_cast<long long>(m_elapsed / 1000000000ll);
-			long long		milliseconds = static_cast<long long>((m_elapsed - (seconds * 1000000000ll)) / 1000000ll);
-			long long		microseconds = static_cast<long long>((m_elapsed - (seconds * 1000000000ll) - (milliseconds * 1000000ll)) / 1000ll);
-			long long		nanoseconds = static_cast<long long>(m_elapsed - (seconds * 1000000000ll) - (milliseconds * 1000000ll) - (microseconds * 1000ll));
+			long long		nanoseconds = m_elapsed;
 
-			std::cout << "Elapsed time: " << seconds << " s, " << milliseconds << " ms, " << microseconds << " us, " << nanoseconds << " ns." << std::endl;
+			// Conversion factors between each time unit
+			double			nanoToSec = 0.000000001, nanoToMilli = 0.000001, nanoToMicro = 0.001;
+			long long		secToNano = 1000000000ll, milliToNano = 1000000ll, microToNano = 1000ll;
+
+			// Divide into microseconds, milliseconds, etc
+			long long		seconds = static_cast<long long>(nanoseconds * nanoToSec);
+
+			// Keep remainder
+			nanoseconds -= seconds * secToNano;
+
+			long long		milliseconds = static_cast<long long>(nanoseconds * nanoToMilli);
+
+			nanoseconds -= milliseconds * milliToNano;
+
+
+			long long		microseconds = static_cast<long long>(nanoseconds * nanoToMicro);
+
+			nanoseconds -= microseconds * microToNano;
+
+
+			std::cout << "Elapsed time: " << seconds << " s, " << milliseconds << " ms, " << microseconds << " us, " << nanoseconds << " ns." << '\n';
 		}
 	}
 
-	/// @brief Get elapsed time in nanoseconds (must be previously saved with start() and stop() or getRuntime())
-	/// @return : Nanoseconds between start and stop (long long)
-	long long getClock(void) { return m_elapsed; }
+	// Get elapsed time in nanoseconds (must be previously saved with start() and stop() or getRuntime())
+	long long getClock(void) const { return m_elapsed; }
 
-
-	/// @brief Record a function's execution time
-	/// @tparam T : Function's return type
-	/// @tparam ...Args : Argument types to pass to function to test
-	/// @param func : Function pointer to test
-	/// @param ...args : Arguments to pass to func
-	/// @return Recorded time in nanoseconds
-	template<typename T, typename... Args>
-	long long getRuntime(T(*func)(Args...), Args... args)
-	{
-		// Get timer ready and start
-		clear();
-		start();
-
-		// Run function to test
-		func(args...);
-
-		// Stop timer
-		stop();
-		return m_elapsed;
-	}
 
 
 
