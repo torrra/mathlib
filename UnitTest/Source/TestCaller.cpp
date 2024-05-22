ba105634ec136ce5a02ae9b38d482979025157c6
Noah DE PISCHOF <141101956+torrra@users.noreply.github.com>
2024-05-22 15:02:03 -0400

    FIX TYPO IN TESTCALLER.CPP
    Fix typo in commented out push_back call in
    AddTests function

--- UnitTest/Source/TestCaller.cpp
+++ UnitTest/Source/TestCaller.cpp
@@ -4,8 +4,8 @@ void AddTests(std::vector<const char*>& tests)
 {
     tests.push_back("[all]");
 
-    // tests.push_bacl("[matrix]);
-    // tests.push_back("[angle]");
+    // tests.push_back("[matrix]);
+    //tests.push_back("[angle]");
     // tests.push_back("[vector]");
 
     // tests.push_back("[intersection]");
