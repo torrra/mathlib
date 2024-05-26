932692bbe80bce723e4455d18075de6f2b48666c
Noah DE PISCHOF <141101956+torrra@users.noreply.github.com>
2024-05-26 14:38:51 -0400

    APPLY PASCALCASE TO TRIGONOMETRY
    Rename Trigonometry functions to apply
    PascalCase in Trigonometry.h and
    Trigonometry.cpp

--- libmath/Header/libmath/Trigonometry.h
+++ libmath/Header/libmath/Trigonometry.h
@@ -4,18 +4,18 @@
 
 namespace mth
 {
-	float		cos(const Radian& _rad);
+	float		Cos(const Radian& _rad);
 
-	float		sin(const Radian& _rad);
+	float		Sin(const Radian& _rad);
 
-	float		tan(const Radian& _rad);
+	float		Tan(const Radian& _rad);
 
 
-	Radian		acos(float _cosine);
-	Radian		asin(float _sine);
-	Radian		atan(float _tangent);
+	Radian		Acos(float _cosine);
+	Radian		Asin(float _sine);
+	Radian		Atan(float _tangent);
 
-	Radian		atan2(float _tangent);
+	Radian		Atan2(float _tangent);
 
 
 
@@ -26,9 +26,7 @@ namespace mth
 				Factorials(void) = default;
 
 
-		static const float		evenFacts[6];
-		static const float		oddFacts[6];
-		static const float		acosFactors[6];
+		static const float		m_evenFacts[6];
 	};
 }
 
--- libmath/Source/Trigonometry.cpp
+++ libmath/Source/Trigonometry.cpp
@@ -3,19 +3,19 @@
 
 #include <cmath>
 
-#define COS_IT 6u
+#define COS_IT		6u
 
 namespace mth
 {
 	static float cosFlt(float _val)
 	{
-		float powResult, cosResult = 0.f, itResult, radians = wrap(_val, -MTH_PI, MTH_PI);
+		float powResult, cosResult = 0.f, itResult, radians = Wrap(_val, -MTH_PI, MTH_PI);
 
 		for (unsigned int it = 1; it <= COS_IT; ++it)
 		{
-			powResult = pow(radians, it * 2);
+			powResult = Pow(radians, it * 2);
 
-			itResult = powResult * mth::Factorials::evenFacts[it - 1];
+			itResult = powResult * mth::Factorials::m_evenFacts[it - 1];
 
 			cosResult += itResult;
 
@@ -26,15 +26,15 @@ namespace mth
 		return cosResult;
 	}
 
-	float cos(const Radian& _rad)
+	float Cos(const Radian& _rad)
 	{
 		float powResult, cosResult = 0.f, radians = _rad.Rad(true), itResult;
 
 		for (unsigned int it = 1; it <= COS_IT; ++it)
 		{
-			powResult = pow(radians, it * 2);
+			powResult = Pow(radians, it * 2);
 
-			itResult = powResult * mth::Factorials::evenFacts[it - 1];
+			itResult = powResult * mth::Factorials::m_evenFacts[it - 1];
 
 			cosResult += itResult;
 
@@ -45,13 +45,13 @@ namespace mth
 		return cosResult;
 	}
 
-	float sin(const Radian& _rad)
+	float Sin(const Radian& _rad)
 	{
 		// Apply 90 degree offset
 		// See: https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Sine_cosine_one_period.svg/1920px-Sine_cosine_one_period.svg.png
 		constexpr float		piOverTwo = 1.570796326794896619231f;
 
-		float				cosine = cosFlt(absolute(_rad.Raw()) - piOverTwo);
+		float				cosine = cosFlt(Absolute(_rad.Raw()) - piOverTwo);
 
 		if (_rad.Raw() < 0.f)
 			return -cosine;
@@ -59,38 +59,38 @@ namespace mth
 		else return cosine;
 	}
 
-	float tan(const Radian& _rad)
+	float Tan(const Radian& _rad)
 	{
 		// tan x = sin x / cos x
-		return sin(_rad) / cos(_rad);
+		return Sin(_rad) / Cos(_rad);
 	}
 
 	// TODO: re-implement my own inverse tring functions
 
-	Radian acos(float _cosine)
+	Radian Acos(float _cosine)
 	{
 		return Radian(std::acosf(_cosine));
 	}
 
-	Radian asin(float _sine)
+	Radian Asin(float _sine)
 	{
 		return Radian(std::asinf(_sine));
 	}
 
-	Radian atan(float _tangent)
+	Radian Atan(float _tangent)
 	{
 		return Radian(std::atanf(_tangent));
 	}
 
-	const float mth::Factorials::evenFacts[] =
+	const float mth::Factorials::m_evenFacts[] =
 	{
 			// Pre-compute even factorials and divisions for faster cosine execution
-			1.f / static_cast<float>(factorial(2)),
-			-1.f / static_cast<float>(factorial(4)),
-			1.f / static_cast<float>(factorial(6)),
-			-1.f / static_cast<float>(factorial(8)),
-			1.f / static_cast<float>(factorial(10)),
-			-1.f / static_cast<float>(factorial(12)),
+			1.f / static_cast<float>(Factorial(2)),
+			-1.f / static_cast<float>(Factorial(4)),
+			1.f / static_cast<float>(Factorial(6)),
+			-1.f / static_cast<float>(Factorial(8)),
+			1.f / static_cast<float>(Factorial(10)),
+			-1.f / static_cast<float>(Factorial(12))
 	};
 
 
