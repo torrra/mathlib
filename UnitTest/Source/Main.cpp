e4707f8ef2f987c04a89e4b3cf1feaad6c1c022b
Noah DE PISCHOF <141101956+torrra@users.noreply.github.com>
2024-05-26 14:49:08 -0400

    ADD COMMENTS TO MAIN.CPP
    Add comments to  main function

--- UnitTest/Source/Main.cpp
+++ UnitTest/Source/Main.cpp
@@ -1,19 +1,20 @@
-#include <catch2/catch_session.hpp>
-
-#include "TestCaller.h"
-
 #include <iostream>
 
-
+#include <catch2/catch_session.hpp>
 
 #if (WIN32)
+
+#include "TestCaller.h"
 
 int main(void)
 {
 	std::vector<const char*>	arguments;
 
+	// Avoid using Visual Studio's command line tool,
+	// manually add relevant tests
 	AddTests(arguments);
 
+	// Get address of first char array to use as char* argv[]
 	return Catch::Session().run((int)arguments.size(), &arguments[0]);
 }
 
@@ -21,6 +22,8 @@ int main(void)
 
 int main(int argc, char* argv[])
 {
+	// Get arguments from the command line if not
+	// running on Windows
 	return Catch::Session().run(argc, argv);
 }
 
