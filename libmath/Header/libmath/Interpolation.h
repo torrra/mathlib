9f9615c77123992ce5bd8864ab42d131ac5415eb
Noah DE PISCHOF <141101956+torrra@users.noreply.github.com>
2024-06-05 13:33:43 -0400

    ADD COMMENTS TO INTERPOLATION.H
    Comment interpolation function declarations

--- libmath/Header/libmath/Interpolation.h
+++ libmath/Header/libmath/Interpolation.h
@@ -5,7 +5,11 @@
 
 namespace mth
 {
+	// Linear interpolation bewtween a start, an end and a ratio
+	// between 0 and 1
 	float		Lerp(float _low, float _high, float _ratio);
 
+
+	// Get the Lerp ratio from a lerp'ed value and its bounds
 	float		InverseLerp(float _val, float _low, float _high);
 }
