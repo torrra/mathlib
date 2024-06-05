73b0a7f913e8e1e56869aabe168cf2bc6c448e49
Noah DE PISCHOF <141101956+torrra@users.noreply.github.com>
2024-06-05 14:38:27 -0400

    ADD COMMENTS TO TRIGONOMETRY.H
    Add comments to tigonometry function declarations

--- libmath/Header/libmath/Trigonometry.h
+++ libmath/Header/libmath/Trigonometry.h
@@ -8,21 +8,28 @@
 
 namespace mth
 {
+	// Compute cosine of an angle in radians using
+	// 6 iterations of Taylor's series
 	float		Cos(const Radian& _rad);
 
+	// Compute sine using Cos with a pi / 2 offset
 	float		Sin(const Radian& _rad);
 
+	// Compute tan using sin / cos
 	float		Tan(const Radian& _rad);
 
+	// Inverse trigonometry
 
 	Radian		Acos(float _cosine);
 	Radian		Asin(float _sine);
 	Radian		Atan(float _tangent);
 
+	// Compute arctangent by dividing y by x
+	// Note: can handle x being equal to 0
 	Radian		Atan2(float _y, float _x);
 
 
-
+	// Class made to hold factorials for cos
 	class Factorials
 	{
 	public:
