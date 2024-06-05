bf122216901a5f61fdd7dfc9780b3d92bb8ba5e7
Noah DE PISCHOF <141101956+torrra@users.noreply.github.com>
2024-06-05 15:57:30 -0400

    ADD COMMENTS TO INTERPOLATION.CPP
    Add comments to Interpolation function bodies

--- libmath/Source/Interpolation.cpp
+++ libmath/Source/Interpolation.cpp
@@ -10,11 +10,13 @@ namespace mth
 		else if (_ratio == 1.f)
 			return _high;
 
+		// Use naive lerp formula
 		return (1.f - _ratio) * _low + _ratio * _high;
 	}
 
 	float InverseLerp(float _val, float _low, float _high)
 	{
+		// Reverse lerp formula to get ratio (commonly refered to as t)
 		return (_val - _low) / (_high - _low);
 	}
 
