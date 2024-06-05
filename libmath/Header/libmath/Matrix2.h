4fed41970f0cdc8b18be29323f61a8449ed797e6
Noah DE PISCHOF <141101956+torrra@users.noreply.github.com>
2024-06-05 14:17:00 -0400

    ADD COMMENTS TO MATRIX2.H
    Add comments to Matrix2 class definition

--- libmath/Header/libmath/Matrix2.h
+++ libmath/Header/libmath/Matrix2.h
@@ -8,6 +8,8 @@ namespace mth
     {
     public:
 
+        // Constructor / destructor
+
                         Matrix2(void) = default;
                         Matrix2(const float _diag);
                         Matrix2(float _a, float _b, float _c, float _d);
@@ -16,21 +18,33 @@ namespace mth
 
                         ~Matrix2(void) = default;
 
+        // Calculate matrix determinant
         float           Determinant(void)                   const;
 
+        // Turn this matrix into an identity matrix of a given diagonal
         void            Identity(float _diag = 1.f);
 
+        // Get transposed matrix of cofactors
         Matrix2         Adjugate(void)                      const;
+
+        // Get inverse of this matrix
         Matrix2         Inverse(void)                       const;
+
+        // Get transposed copy of this matrix
         Matrix2         Transpose(void)                     const;
 
         Matrix2&        operator=(const Matrix2& _rhs);
+
+        // Matrix / matrix operators
 
         Matrix2         operator+(const Matrix2& _rhs)      const;
         Matrix2         operator-(const Matrix2& _rhs)      const;
         Matrix2         operator*(const Matrix2& _rhs)      const;
 
+        // Multiply all numbers of a copy of this matrix by a scalar factor
         Matrix2         operator*(float _factor)            const;
+
+        // MAtrix / matrix assignment operators
 
         Matrix2&        operator+=(const Matrix2& _rhs);
         Matrix2&        operator-=(const Matrix2& _rhs);
@@ -39,10 +53,13 @@ namespace mth
         bool            operator==(const Matrix2& _rhs)     const;
         bool            operator!=(const Matrix2& _rhs)     const;
 
+        // Access a row (no bound checking)
         float*          operator[](int _index);
+
+        // Access a row (no bound checking)
         float const*    operator[](int _index)              const;
 
-
+        // Get a 2x2 rotation matrix from an angle in radians
         static Matrix2  Rotate(Radian _angle);
 
     private:
