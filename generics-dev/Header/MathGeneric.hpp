5f718ad3116daf018bb225c989a237b6a1dc86d6
Torra <noah.dpschf@gmail.com>
2024-06-22 15:14:28 -0400

    CREATE MATH GENERIC FILE
    Create header with template definitions for math types

---
+++ generics-dev/Header/MathGeneric.hpp
@@ -0,0 +1,64 @@
+/*
+
+ _____                               _
+|_   _|                             (_)
+  | |  ___  _ __     ___ _ __   __ _ _ _ __   ___
+  | | / _ \| '_ \   / _ \ '_ \ / _` | | '_ \ / _ \
+ _| || (_) | | | | |  __/ | | | (_| | | | | |  __/
+ \___/\___/|_| |_|  \___|_| |_|\__, |_|_| |_|\___|
+                                __/ |
+                               |___/
+
+
+NAME: MathGenerics.h
+
+DESCTIPTION: Template definitions for mathematical classes
+
+AUTHOR: Noah de Pischof | @torrra on GitHub
+
+
+STYLE:
+
+C prefix: C++20 template concept
+T prefix: Template parameter
+
+Type suffix: Generic type, goes with T prefix
+
+*/
+
+#include <type_traits>
+
+namespace ion
+{
+  namespace math
+  {
+    // Constrain type to a number:
+    // int, float, etc... excluding pointers
+    template <typename TEvaluatedType>
+    concept CScalarType =
+    std::is_arithmetic<TEvaluatedType>::value;
+
+
+    // Constrain type to a type that can be manipulated through addition,
+    // subtraction, and multiplication (between two object of this type).
+    // Does NOT account for division. Use CDivisibleObject
+    template <typename TEvaluatedType>
+    concept CArithmeticType = requires (TEvaluatedType a, TEvaluatedType b)
+    {
+      a + b;
+      a - b;
+      a * b;
+    };
+
+
+    // Constrain type to one that can be added, subtracted, multiplied,
+    // AND divided (applies to two objects of the same type)
+    template <typename TEvaluatedType>
+    concept CDivisibleType = CArithmeticType<TEvaluatedType> &&
+    requires (TEvaluatedType a, TEvaluatedType b)
+    {
+      a / b;
+    };
+
+  }
+}
