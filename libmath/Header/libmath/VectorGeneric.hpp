bd0cc3a82d65967e125056991bcbac57bf5d03b3
Torra <noah.dpschf@gmail.com>
2024-06-26 16:51:41 -0400

    CREATE VECTOR GENERIC HEADER
    Create Vector class template and define Vector2 and Vector3
    as partial specializations of that class

---
+++ libmath/Header/libmath/VectorGeneric.hpp
@@ -0,0 +1,72 @@
+/*
+
+ _____                               _
+|_   _|                             (_)
+  | |  ___  _ __     ___ _ __   __ _ _ _ __   ___
+  | | / _ \| '_ \   / _ \ '_ \ / _` | | '_ \ / _ \
+ _| || (_) | | | | |  __/ | | | (_| | | | | |  __/
+ \___/\___/|_| |_|  \___|_| |_|\__, |_|_| |_|\___|
+                                __/ |
+                               |___/
+
+
+NAME: VectorGeneric.hpp
+
+DESCTIPTION: Template definitions for Vector mathematical classes
+
+AUTHOR: Noah de Pischof | @torrra on GitHub
+
+
+STYLE:
+
+C prefix: C++20 template concept
+T prefix: Template parameter
+
+Type suffix: Generic type, goes with T prefix
+
+*/
+
+
+#ifndef __VECTOR_GENERIC_H__
+#define __VECTOR_GENERIC_H__
+
+
+#include <inttypes.h>
+
+#include "libmath/MathGeneric.hpp"
+
+namespace ion::math
+{
+    // Generic vector class , TDimentions defines the number of components (x, y, z...)
+    // TValueType the type of the data it contains
+    template <uint8_t TDimensions, CScalarType TValueType>
+	class Vector;
+
+    // Partial Vector specialization for 2D vector 
+    template <CScalarType TValueType>
+    using Vector2 = Vector<2, TValueType>;
+
+
+    // Vector2 aliases for quick use
+
+    using Vector2f = Vector2<float>;
+    using Vector2d = Vector2<double>;
+    using Vector2i = Vector2<int32_t>;
+
+
+    // Partial specialization for 3D vector
+	template <CScalarType TValueType>
+	using Vector3 = Vector<3, TValueType>;
+
+
+    // Vector3 aliases for quick use
+
+	using Vector3f = Vector3<float>;
+	using Vector3i = Vector3<int32_t>;
+	using Vector3d = Vector3<double>;
+
+}
+
+
+
+#endif
